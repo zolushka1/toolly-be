941442cf9c54f00966bd57e908143b75863578d8
zoljargal olziibat <50563505+zolushka1@users.noreply.github.com>
2023-12-09 22:34:47 +0800

    UserService added repo

--- src/main/java/com/pi/toolly/service/UserService.java
+++ src/main/java/com/pi/toolly/service/UserService.java
@@ -1,7 +1,14 @@
 package com.pi.toolly.service;
 
+import com.pi.toolly.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+
 }
