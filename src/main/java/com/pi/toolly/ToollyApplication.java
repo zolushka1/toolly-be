3288f44ae8ce7dfe9fdf98c869ad627a3d6c64bc
zoljargal olziibat <50563505+zolushka1@users.noreply.github.com>
2023-12-09 18:55:19 +0800

    first initialize

---
+++ src/main/java/com/pi/toolly/ToollyApplication.java
@@ -0,0 +1,13 @@
+package com.pi.toolly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToollyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToollyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pi/toolly/ToollyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.toolly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToollyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
